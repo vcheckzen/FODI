ea099b6734222a4b735a047563e7c66f6c852212
vcheckzen <18008498+vcheckzen@users.noreply.github.com>
2024-08-20 02:13:33 +0800

    feat: add deployment helper

---
+++ back-end-deployment-helper/src/App.js
@@ -0,0 +1,229 @@
+import React, {useRef, useState} from 'react';
+import {Button, Select, Input, Alert, Spin, message} from 'antd';
+import {LoadingOutlined} from '@ant-design/icons';
+
+import './App.css';
+import {generateCode} from "./util";
+
+const defaultConfig = {
+  replayURL: 'http://localhost/onedrive-login',
+  publicParams: '&scope=offline_access%20User.Read%20Files.ReadWrite.All&response_type=code',
+  version: {
+    cn: {
+      api: 'https://login.partner.microsoftonline.cn',
+      restApi: 'https://microsoftgraph.chinacloudapi.cn',
+      clientID: 'dfe36e60-6133-48cf-869f-4d15b8354769',
+      clientSecret: 'H0-1:6.Sb8:WCW/J-c]K@fddCt[i0EZ2',
+    },
+    other: {
+      api: 'https://login.microsoftonline.com',
+      restApi: 'https://graph.microsoft.com',
+      clientID: '78d4dc35-7e46-42c6-9023-2d39314433a5',
+      clientSecret: 'ZudGl-p.m=LMmr3VrKgAyOf-WevB3p50',
+    }
+  },
+  reverseProxyURL: 'https://www.reverseproxy.com',
+};
+
+function App() {
+  const {Option} = Select;
+  const antIcon = <LoadingOutlined spin/>;
+
+  const [version, setVersion] = useState();
+  const [replayURL, setReplayURL] = useState();
+  const [clientID, setClientID] = useState();
+  const [clientSecret, setClientSecret] = useState();
+  const [redirectURL, setRedirectURL] = useState();
+  const [passwordFilename, setPasswordFilename] = useState();
+  const [exposedPath, setExposedPath] = useState();
+  const [code, setCode] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+  const textAreaRef = useRef();
+
+
+  const changeVersion = (v) => {
+    if (v === 'select') {
+      setVersion(null);
+      setReplayURL(null);
+      setClientID(null);
+      setClientSecret(null);
+      return;
+    }
+    setRedirectURL(null);
+    setVersion(v);
+    const config = defaultConfig.version[v];
+    setReplayURL(defaultConfig.replayURL);
+    setClientID(config.clientID);
+    setClientSecret(config.clientSecret);
+  };
+
+  const login = () => {
+    if (!version || version === 'select') return;
+    const config = defaultConfig.version[version];
+    window.open(
+      `${config.api}/common/oauth2/v2.0/authorize?client_id=` +
+      `${clientID}${defaultConfig.publicParams}&redirect_uri=${replayURL}`
+    );
+  };
+
+  const getCode = () => {
+    if (!version || !redirectURL || version === 'select') return;
+    let code;
+    try {
+      code = new URLSearchParams(new URL(redirectURL).search).get('code');
+    } catch (e) {
+      setError('跳转地址错误');
+      return;
+    }
+    setError(null);
+    setLoading(true);
+
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("client_id", clientID);
+    urlencoded.append("redirect_uri", replayURL);
+    urlencoded.append("client_secret", clientSecret);
+    urlencoded.append("code", code);
+    urlencoded.append("grant_type", "authorization_code");
+
+    const requestOptions = {
+      method: 'POST',
+      headers: headers,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+
+    fetch(`${defaultConfig.reverseProxyURL}?url=${defaultConfig.version[version].api}/common/oauth2/v2.0/token`
+      , requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setLoading(null);
+        if ('error' in data) {
+          setError(data.error_description);
+        } else {
+          setCode(generateCode(
+            defaultConfig.version[version].api,
+            defaultConfig.version[version].restApi,
+            clientID,
+            clientSecret,
+            replayURL,
+            data.refresh_token,
+            exposedPath || '',
+            passwordFilename || '.password',
+          ))
+        }
+      })
+      .catch(error => {
+        setLoading(null);
+        setError(error);
+      });
+  };
+
+  const copyCode = (e) => {
+    if (!code) return;
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    e.target.focus();
+    message.success('复制成功')
+  };
+
+  return (
+    <div className="main">
+
+      {loading && <Spin className="progress" indicator={antIcon}/>}
+      {error && <Alert
+        message="Error"
+        description={error}
+        type="error"
+        showIcon
+        closable
+      />}
+
+      <div>
+        <div className="header">
+          FODI DEPLOYMENT HELPER
+        </div>
+
+        <div className="content">
+          <div className="steps">
+            <ul>
+              <li>选择版本，点击按钮完成登录，登录成功会跳转到 localhost</li>
+              <li>此时复制地址栏链接粘贴到下方 “浏览器跳转地址” 输入框</li>
+              <li>点击获取代码，代码出现后，点击复制代码</li>
+              <li>打开 Cloudflare Worker 编辑器，覆盖粘贴原有代码保存</li>
+            </ul>
+          </div>
+
+          <div className="input version">
+            <Select
+              size="large"
+              defaultValue="select"
+              onChange={(v) => changeVersion(v)}
+            >
+              <Option value="select">请选择版本</Option>
+              <Option value="cn">世纪互联</Option>
+              <Option value="other">其他版本</Option>
+            </Select>
+            <Input
+              placeholder="CLIENT_ID"
+              value={clientID}
+              onChange={(e) => setClientID(e.target.value)}
+            />
+            <Input
+              placeholder="CLIENT_SECRET"
+              value={clientSecret}
+              onChange={(e) => setClientSecret(e.target.value)}
+            />
+          </div>
+
+          <div className="input between">
+            <Input
+              placeholder="ReplayURL（自定义 ID、SECRET 时，需要填写）"
+              value={replayURL}
+              onChange={(e) => setReplayURL(e.target.value)}
+            />
+            <Button onClick={login}>前往登录</Button>
+          </div>
+
+          <div className="input">
+            <Input
+              placeholder="浏览器跳转地址"
+              value={redirectURL}
+              onChange={(e) => setRedirectURL(e.target.value)}
+            />
+          </div>
+
+          <div className="input between">
+            <Input
+              placeholder="展示文件夹（默认根路径）"
+              value={exposedPath}
+              onChange={(e) => setExposedPath(e.target.value)}
+            />
+            <Input
+              placeholder="密码文件名（默认 .password）"
+              value={passwordFilename}
+              onChange={(e) => setPasswordFilename(e.target.value)}
+            />
+          </div>
+
+          <div className="input between">
+            <Button onClick={getCode}>获取代码</Button>
+            <Button onClick={copyCode}>复制代码</Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="code">
+          <textarea value={code} ref={textAreaRef} onChange={(e) => setCode(e.target.value)}>
+          </textarea>
+      </div>
+      <div className="footer">FODI © {new Date().getFullYear()} </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ back-end-deployment-helper/src/util.js
@@ -0,0 +1,217 @@
+export const generateCode = (
+  loginHost,
+  apiHost,
+  clientId,
+  clientSecret,
+  replayURL,
+  refreshToken,
+  exposePath,
+  passwordFilename
+) => {
+  return `const EXPOSE_PATH = "${exposePath}";
+const ONEDRIVE_REFRESHTOKEN = "${refreshToken}";
+const PASSWD_FILENAME = "${passwordFilename}";
+const clientId = "${clientId}";
+const clientSecret = "${clientSecret}";
+const loginHost = "${loginHost}";
+const apiHost = "${apiHost}";
+const redirectUri = "${replayURL}";
+
+addEventListener('scheduled', event => {
+  event.waitUntil(fetchAccessToken(event.scheduledTime));
+});
+
+addEventListener("fetch", (event) => {
+  try {
+    return event.respondWith(handleRequest(event.request));
+  } catch (e) {
+    return event.respondWith(new Response("Error thrown " + e.message));
+  }
+});
+
+const OAUTH = {
+  redirectUri: redirectUri,
+  refreshToken: ONEDRIVE_REFRESHTOKEN,
+  clientId: clientId,
+  clientSecret: clientSecret,
+  oauthUrl: loginHost + "/common/oauth2/v2.0/",
+  apiUrl: apiHost + "/v1.0/me/drive/root",
+  scope: apiHost + "/Files.ReadWrite.All offline_access",
+};
+
+async function handleRequest(request) {
+  let querySplited, requestPath;
+  let queryString = decodeURIComponent(request.url.split("?")[1]);
+  if (queryString) querySplited = queryString.split("=");
+  if (querySplited && querySplited[0] === "file") {
+    const file = querySplited[1];
+    const fileName = file.split("/").pop();
+    if (fileName === PASSWD_FILENAME)
+      return Response.redirect(
+        "https://www.baidu.com/s?wd=%E6%80%8E%E6%A0%B7%E7%9B%97%E5%8F%96%E5%AF%86%E7%A0%81",
+        301
+      );
+    requestPath = file.replace("/" + fileName, "");
+    const url = await fetchFiles(requestPath, fileName);
+    return Response.redirect(url, 302);
+  } else {
+    const { headers } = request;
+    const contentType = headers.get("content-type");
+    let body = {};
+    if (contentType && contentType.includes("form")) {
+      const formData = await request.formData();
+      for (let entry of formData.entries()) {
+        body[entry[0]] = entry[1];
+      }
+    }
+    requestPath = Object.getOwnPropertyNames(body).length ? body["?path"] : "";
+    const files = await fetchFiles(requestPath, null, body.passwd);
+    return new Response(files, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Cache-Control": "max-age=3600",
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
+  }
+}
+
+async function gatherResponse(response) {
+  const { headers } = response;
+  const contentType = headers.get("content-type");
+  if (contentType.includes("application/json")) {
+    return await response.json();
+  } else if (contentType.includes("application/text")) {
+    return await response.text();
+  } else if (contentType.includes("text/html")) {
+    return await response.text();
+  } else {
+    return await response.text();
+  }
+}
+
+async function cacheFetch(url, options) {
+  return fetch(new Request(url, options), {
+    cf: {
+      cacheTtl: 3600,
+      cacheEverything: true,
+    },
+  });
+}
+
+async function getContent(url) {
+  const response = await cacheFetch(url);
+  const result = await gatherResponse(response);
+  return result;
+}
+
+async function getContentWithHeaders(url, headers) {
+  const response = await cacheFetch(url, { headers: headers });
+  const result = await gatherResponse(response);
+  return result;
+}
+
+async function fetchFormData(url, data) {
+  const formdata = new FormData();
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      formdata.append(key, data[key]);
+    }
+  }
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+  };
+  const response = await cacheFetch(url, requestOptions);
+  const result = await gatherResponse(response);
+  return result;
+}
+
+async function fetchAccessToken() {
+  let refreshToken = OAUTH["refreshToken"];
+  if (typeof FODI_CACHE !== 'undefined') {
+    const cache = JSON.parse(await FODI_CACHE.get('token_data'));
+    if (cache?.refresh_token) {
+      const passedMilis = Date.now() - cache.save_time;
+      if (passedMilis / 1000 < cache.expires_in - 600) {
+        return cache.access_token;
+      }
+
+      if (passedMilis < 6912000000) {
+        refreshToken = cache.refresh_token;
+      }
+    }
+  }
+
+  const url = OAUTH["oauthUrl"] + "token";
+  const data = {
+    client_id: OAUTH["clientId"],
+    client_secret: OAUTH["clientSecret"],
+    grant_type: "refresh_token",
+    requested_token_use: "on_behalf_of",
+    refresh_token: refreshToken,
+  };
+  const result = await fetchFormData(url, data);
+
+  if (typeof FODI_CACHE !== 'undefined' && result?.refresh_token) {
+    result.save_time = Date.now();
+    await FODI_CACHE.put('token_data', JSON.stringify(result));
+  }
+  return result.access_token;
+}
+
+async function fetchFiles(path, fileName, passwd) {
+  if (path === "/") path = "";
+  if (path || EXPOSE_PATH) path = ":" + EXPOSE_PATH + path;
+
+  const accessToken = await fetchAccessToken();
+  const uri =
+    OAUTH.apiUrl +
+    encodeURI(path) +
+    "?expand=children(select=name,size,parentReference,lastModifiedDateTime,@microsoft.graph.downloadUrl)";
+  const body = await getContentWithHeaders(uri, {
+    Authorization: "Bearer " + accessToken,
+  });
+  if (fileName) {
+    let thisFile = null;
+    body.children.forEach((file) => {
+      if (file.name === decodeURIComponent(fileName)) {
+        thisFile = file["@microsoft.graph.downloadUrl"];
+        return;
+      }
+    });
+    return thisFile;
+  } else {
+    let files = [];
+    let encrypted = false;
+    for (let i = 0; i < body.children.length; i++) {
+      const file = body.children[i];
+      if (file.name === PASSWD_FILENAME) {
+        const PASSWD = await getContent(file["@microsoft.graph.downloadUrl"]);
+        if (PASSWD !== passwd) {
+          encrypted = true;
+          break;
+        } else {
+          continue;
+        }
+      }
+      files.push({
+        name: file.name,
+        size: file.size,
+        time: file.lastModifiedDateTime,
+        url: file["@microsoft.graph.downloadUrl"],
+      });
+    }
+    let parent = body.children.length
+      ? body.children[0].parentReference.path
+      : body.parentReference.path;
+    parent = parent.split(":").pop().replace(EXPOSE_PATH, "") || "/";
+    parent = decodeURIComponent(parent);
+    if (encrypted) {
+      return JSON.stringify({ parent: parent, files: [], encrypted: true });
+    } else {
+      return JSON.stringify({ parent: parent, files: files });
+    }
+  }
+}`;
+};
